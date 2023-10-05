3378c1ca8334b22730cba3c0216595f4168b052d
Arafat <mdarafathosainchowdhory@gmail.com>
2023-10-06 00:02:18 +0600

    test

---
+++ Comment.py
@@ -0,0 +1,18 @@
+#Hey Arafat dont remove this line
+print("Hey I am a \ngood boy")
+#\n is a scape secuence charector
+# # for comment
+
+print("Hello World")
+
+'''My name is
+Arafat'''
+"""Comment.py"""
+
+print("Hey I am a \"good boy\" \nand this viewer is also a good boy/girl")
+
+print("Hey", 6, 8, sep="~", end="009")
+print("Arafat")
+
+print("Hey", 6, 8, sep="~", end="009\n")
+print("Arafat")
