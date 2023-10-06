83febac74e78c68f77df0ee077a7973c09ee6774
Md. Arafat Hossain Chowdhory <142386561+Arafat5111@users.noreply.github.com>
2023-10-06 19:12:58 +0600

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,24 @@
+a = 1.1
+b = True
+c = "Arafat"
+d = None
+a1 = 9
+a2 = complex(8, 2)
+print(a)
+print(b)
+print(a + a1)
+print("The type of a is", type(a))
+print("The type of a1 is", type(a1))
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+print("The type of d is", type(d))
+print("The type of a2 is", type(a2))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Arafat", "age":20, "canVote":True}
+print(dict1)
