dc1e682726a0fc6ff86799db446a29cbf4adaec2
Md. Arafat Hossain Chowdhory <142386561+Arafat5111@users.noreply.github.com>
2023-10-07 21:52:48 +0600

    Add files via upload

---
+++ String_Slicing.py
@@ -0,0 +1,16 @@
+name = ("Arafat,Alvee")
+print(name[0:6])
+print(len(name))
+
+fruit = "Mango"
+len1 = len(fruit)
+print("Mango is a", len1, "letter word")
+print(fruit[0:4]) # Including 0 but not 4
+print(fruit[1:4])  # Including 1 but not 4
+print(fruit[:])
+print(fruit[:-3])
+print(fruit[-3:-1])
+
+# Quick Quiz:
+nm = "Alvee"
+print(nm[-4:-2])
