20f14db07c726c6f16cec135ba946b911e358dd6
Arafat <mdarafathosainchowdhory@gmail.com>
2023-10-08 15:39:53 +0600

    Break Continue

---
+++ 19Break.py
@@ -0,0 +1,16 @@
+for i in range(12):
+    
+    if(i == 10):
+        print("Skip the itretion")
+        # continue
+        break
+    print("5 X", i+1, "=", 5 * (i+1))
+
+
+i = 0 #Do While Loop
+while True:
+    print(i)
+    i = i+1
+    if(i%100 == 0):
+        break
+
