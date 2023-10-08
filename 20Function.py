60fb8db669e77446f32bb6d79b07f0690c028a30
Arafat <mdarafathosainchowdhory@gmail.com>
2023-10-08 16:11:05 +0600

    Function

---
+++ 20Function.py
@@ -0,0 +1,27 @@
+def calculateGmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a, b):
+    if(a>b):
+        print("First number is greater")
+    else:
+        print("Second Number is greater")
+
+def islesser(a, b):
+    pass
+
+
+a = 9
+b = 8
+isGreater(a, b)
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+calculateGmean(a, b)
+
+c = 8
+d = 88
+isGreater(c, d)
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
+calculateGmean(c, d)
