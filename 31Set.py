171f0cde24f6c698b7f36965d472d05016f9a887
Arafat <mdarafathosainchowdhory@gmail.com>
2023-10-09 20:29:54 +0600

    Sets

---
+++ 31Set.py
@@ -0,0 +1,13 @@
+s = {2, 4, 2, 6}
+print(s) # Value will not repeatet
+print(type(s))
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info) # Sequence will not fixed
+
+arafat = set()
+print(arafat)
+print(type(arafat))
+
+for value in info:
+    print(value)
