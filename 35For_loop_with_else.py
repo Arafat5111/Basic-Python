ff8735ba70d5a7730019f70f9bd09b9ef70abc7a
Arafat <mdarafathosainchowdhory@gmail.com>
2023-10-09 21:46:16 +0600

    For Loop With Else

---
+++ 35For_loop_with_else.py
@@ -0,0 +1,15 @@
+for i in range(6):
+# for i in []:
+    print(i)
+    if i == 4:
+    # if i == 7:
+        break
+else:
+    print("Sorry no i")
+
+
+for x in range(5):
+    print ("iteration no {} in for loop".format(x+1))
+else:
+    print ("else block in loop")
+print ("Out of loop")
