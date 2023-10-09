0d68c87f2556c98a8e9d5cfd65e7c17d42d9425f
Arafat <mdarafathosainchowdhory@gmail.com>
2023-10-09 21:18:12 +0600

    Dictionaries

---
+++ 33Dictionaries.py
@@ -0,0 +1,24 @@
+dic = {
+  "Arafat": "Human being",
+  "Spoon": "Object"  
+}
+print(dic)
+print(dic["Arafat"])
+
+
+dic2 = {
+  344: "Arafat",
+  56: "Alvee",
+  678: "Zahid",
+  567: "Shakib"
+ }
+print(dic2[344])
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info)
+print(info['name'])
+print(info.get('eligible'))
+print(info.get('eligible2'))
+print(info.keys())
+print(info.values())
+print(info.items())
