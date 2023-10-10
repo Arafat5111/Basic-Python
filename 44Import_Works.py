f18f6989047a2cec8ecebd168192e1294638ad9d
Arafat <mdarafathosainchowdhory@gmail.com>
2023-10-10 12:40:29 +0600

    Import Works

---
+++ 44Import_Works.py
@@ -0,0 +1,33 @@
+  #Once a module is imported, you can use any of the functions and variables defined in the module by using the dot notation. For example, to use the sqrt function from the math module, you would write
+import math
+result = math.sqrt(9)
+print(result)  # Output: 3.0
+
+  #You can also import specific functions or variables from a module using the from keyword. For example, to import only the sqrt function from the math module, you would write
+from math import sqrt
+result = sqrt(9)
+print(result)  # Output: 3.0
+
+   #You can also import multiple functions or variables at once by separating them with a comma
+from math import sqrt, pi
+result = sqrt(9)
+print(result)  # Output: 3.0
+print(pi)  # Output: 3.141592653589793
+
+   #It's also possible to import all functions and variables from a module using the * wildcard. However, this is generally not recommended as it can lead to confusion and make it harder to understand where specific functions and variables are coming from
+from math import *
+result = sqrt(9)
+print(result)  # Output: 3.0
+print(pi)  # Output: 3.141592653589793
+
+   #Python also allows you to rename imported modules using the as keyword. This can be useful if you want to use a shorter or more descriptive name for a module, or if you want to avoid naming conflicts with other modules or variables in your code
+import math as m
+result = m.sqrt(9)
+print(result)  # Output: 3.0
+print(m.pi)  # Output: 3.141592653589793
+
+    #Finally, Python has a built-in function called dir that you can use to view the names of all the functions and variables defined in a module. This can be helpful for exploring and understanding the contents of a new module
+import math
+
+print(dir(math)) #This will output a list of all the names defined in the math module, including functions like sqrt and pi, as well as other variables and constants
+
