aac30c7f9f8ae78e82c27befbff16cce7d3272f1
Arafat <mdarafathosainchowdhory@gmail.com>
2023-10-13 21:39:16 +0600

    List Again

---
+++ List_Again.py
@@ -0,0 +1,20 @@
+words = ["Hello", "world", "!"]
+print(words[0])
+print(words[1])
+print(words[2])
+
+my_list = []
+print(my_list)
+
+number = 1
+my_numbers = [number, 2, 3]
+
+things = ["Numbers", 0, my_numbers, 4.56]
+
+print(things[0])
+print(things[1])
+print(things[2])
+print(things[2][2])
+
+str = "Hello world!"
+print(str[6])
