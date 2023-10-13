9fd57a32519849ae0fc3e4676cd7775ed0f2c46f
Arafat <mdarafathosainchowdhory@gmail.com>
2023-10-13 21:16:33 +0600

    Boolean Logic

---
+++ Boolean_Logic.py
@@ -0,0 +1,19 @@
+1 == 1 and 2 == 2
+# True
+
+1 == 1 or 2 == 3
+# True
+
+not 1 == 1
+# False
+not 1 > 7
+# True
+
+a = 1
+b = 1
+
+c = 5
+d = 10
+
+if a == b and d > c:
+    print("a and b are equal and d is greater than c")
