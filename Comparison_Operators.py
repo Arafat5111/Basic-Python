04d3670b6b2b9e26b65ebee124f94eb68a714f68
Arafat <mdarafathosainchowdhory@gmail.com>
2023-11-18 15:22:58 +0600

    Comparison Operators

---
+++ Comparison_Operators.py
@@ -0,0 +1,9 @@
+message = "Hello World"
+message_2 = "Hello Python"
+
+print(message == message_2)
+print(message != message_2)
+print(message < message_2)
+print(message <= message_2)
+print(message > message_2)
+print(message >= message_2)
